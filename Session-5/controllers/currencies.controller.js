c45c6a3a955aa2280d9011fc12245895542343b5
rohhitrz <rohhit.rz@gmail.com>
2024-06-26 12:12:56 +0530

    commit

---
+++ Session-5/controllers/currencies.controller.js
@@ -0,0 +1,35 @@
+currencyData = require('../currencies.json');
+
+const getCurrencies = (req,res)=>{
+    console.log(req.query);
+    // console.log(req.params.symbol);
+    const {min_value} = req.query;
+    if(min_value){
+       return res.json(currencyData.data.filter((curr)=>{
+            curr.min_size === min_value;
+
+        }))
+}
+
+return res.json(currencyData);
+};
+
+const getCurrencyBySymbol = (req,res)=>{
+    
+    
+    console.log(req.params.symbol);
+
+
+    const reqCurrObj = currencyData.data.find((curr)=> curr.id.toLowerCase() == req.params.symbol);
+
+    if(reqCurrObj === undefined){
+       return  res.status(404).send("wrong request")
+    }
+    
+    res.json(reqCurrObj);
+   
+};
+
+module.exports = {getCurrencies,getCurrencyBySymbol};
+
+
